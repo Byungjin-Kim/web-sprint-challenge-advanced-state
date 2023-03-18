2269f8b0dfe2ae370a1580563b13b649a7bd59a0
Byungjin-Kim <composerjins@gmail.com>
2023-03-18 02:53:11 -0500

    completed Wheel app

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,10 +1,31 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+import {
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM
+} from './action-types'
 
-const initialWheelState = 0
+import { combineReducers } from 'redux';
+
+const initialWheelState = 0;
 function wheel(state = initialWheelState, action) {
-  return state
+  switch (action.type) {
+    case MOVE_CLOCKWISE:
+      return (state === 5 ? state = 0 : state + 1);
+
+    case MOVE_COUNTERCLOCKWISE:
+      return (state === 0 ? state = 5 : state - 1);
+
+    default:
+      return state
+  }
 }
+
+// idx === wheel ? 'B' : null
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
